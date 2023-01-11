036ca5f633fbc94aab088aa57316968152be079e
Uhmopt <qethum@gmail.com>
2023-01-12 04:50:41 +0900

    progress

--- src/basketball/basketball.module.ts
+++ src/basketball/basketball.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { BasketballGameScoreModule } from 'src/basketball-game-score/basketball-game-score.module';
+import { BasketballGameModule } from 'src/basketball-game/basketball-game.module';
 import { BasketballController } from './basketball.controller';
 import { BasketballService } from './basketball.service';
 import { Basketball } from './entities/basketball.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Basketball])],
+  imports: [
+    TypeOrmModule.forFeature([Basketball]),
+    BasketballGameModule,
+    BasketballGameScoreModule,
+  ],
   controllers: [BasketballController],
   providers: [BasketballService],
   exports: [BasketballService],
