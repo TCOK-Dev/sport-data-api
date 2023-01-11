d2e17a31a867401578b8b227410b3beb771ce6dd
Uhmopt <qethum@gmail.com>
2023-01-11 11:29:53 +0900

    [New] dto

--- src/basketball-game-score/dto/create-basketball-game-score.dto.ts
+++ src/basketball-game-score/dto/create-basketball-game-score.dto.ts
@@ -1,1 +1,50 @@
-export class CreateBasketballGameScoreDto {}
+import { IsNumber, IsOptional, IsString } from 'class-validator';
+import { BasketballGame } from 'src/basketball-game/entities/basketball-game.entity';
+
+export class CreateBasketballGameScoreDto {
+  @IsString()
+  @IsOptional()
+  title: string;
+
+  @IsString()
+  @IsOptional()
+  quarter: string;
+
+  @IsNumber()
+  @IsOptional()
+  clock: number;
+
+  @IsString()
+  @IsOptional()
+  awayTeam: string;
+
+  @IsString()
+  @IsOptional()
+  homeTeam: string;
+
+  @IsNumber()
+  @IsOptional()
+  awayScore: number;
+
+  @IsNumber()
+  @IsOptional()
+  homeScore: number;
+
+  @IsString()
+  @IsOptional()
+  awaySpread: string;
+
+  @IsString()
+  @IsOptional()
+  homeSpread: string;
+
+  @IsString()
+  @IsOptional()
+  awayOverUnder: string;
+
+  @IsString()
+  @IsOptional()
+  homeOverUnder: string;
+
+  game: BasketballGame;
+}
--- src/basketball-game/dto/create-basketball-game.dto.ts
+++ src/basketball-game/dto/create-basketball-game.dto.ts
@@ -1,1 +1,53 @@
-export class CreateBasketballGameDto {}
+import { IsNumber, IsOptional, IsString } from 'class-validator';
+import { BasketballGameScore } from 'src/basketball-game-score/entities/basketball-game-score.entity';
+import { Basketball } from 'src/basketball/entities/basketball.entity';
+
+export class CreateBasketballGameDto {
+  @IsString()
+  @IsOptional()
+  title: string;
+
+  @IsString()
+  @IsOptional()
+  quarter: string;
+
+  @IsNumber()
+  @IsOptional()
+  clock: number;
+
+  @IsString()
+  @IsOptional()
+  awayTeam: string;
+
+  @IsString()
+  @IsOptional()
+  homeTeam: string;
+
+  @IsNumber()
+  @IsOptional()
+  awayScore: number;
+
+  @IsNumber()
+  @IsOptional()
+  homeScore: number;
+
+  @IsString()
+  @IsOptional()
+  awaySpread: string;
+
+  @IsString()
+  @IsOptional()
+  homeSpread: string;
+
+  @IsString()
+  @IsOptional()
+  awayOverUnder: string;
+
+  @IsString()
+  @IsOptional()
+  homeOverUnder: string;
+
+  basketball: Basketball;
+
+  scores: BasketballGameScore[];
+}
