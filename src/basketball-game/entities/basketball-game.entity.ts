d57829bc778cbca5ff7289a258bfd368ec69e089
Uhmopt <qethum@gmail.com>
2023-01-14 06:09:12 +0900

    [new] time query

--- src/basketball-game/entities/basketball-game.entity.ts
+++ src/basketball-game/entities/basketball-game.entity.ts
@@ -6,12 +6,27 @@ import {
   Column,
   OneToMany,
   ManyToOne,
+  CreateDateColumn,
+  UpdateDateColumn,
 } from 'typeorm';
 
 @Entity()
 export class BasketballGame {
   @PrimaryGeneratedColumn()
   id: number;
+
+  @CreateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP(6)',
+  })
+  public createdAt: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP(6)',
+    onUpdate: 'CURRENT_TIMESTAMP(6)',
+  })
+  public updatedAt: Date;
 
   @Column({ nullable: true })
   title: string;
